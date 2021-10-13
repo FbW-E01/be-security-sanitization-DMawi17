f3249b98ec5f266d1ebe9f91d9e7d5ca000a91cf
Dagmawi A <75326045+DMawi17@users.noreply.github.com>
2021-10-13 22:50:15 +0200

    Update validation.js

--- validation/validation.js
+++ validation/validation.js
@@ -3,17 +3,17 @@ import { body, check } from "express-validator";
 const rules = [
     body("name")
         .isLength({ min: 2 })
-        .withMessage("min 2 char")
+        .withMessage("Minimum of two characters required!")
         .trim()
         .isAlpha()
-        .withMessage("Use only letters only please!"),
+        .withMessage("Use only letters please!"),
     body("email")
         .isEmail()
         .normalizeEmail()
         .withMessage("Must be a valid email address"),
     body("telephone")
         .isLength({ min: 6 })
-        .withMessage("Minimum of 6 characters!")
+        .withMessage("Minimum of 6 characters required!")
         .isNumeric()
         .withMessage("User only numbers please!"),
     body("country"),
