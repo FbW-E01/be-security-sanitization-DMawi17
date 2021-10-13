9e0a68cc587cf4788e9f404e57096f2b9479cd26
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-13 22:39:40 +0200

    err-msg sent as alert

--- validation/validation.js
+++ validation/validation.js
@@ -1,14 +1,25 @@
 import { body, check } from "express-validator";
 
 const rules = [
-    body("name").isLength({ min: 3 }).trim().isAlpha(),
+    body("name")
+        .isLength({ min: 3 })
+        .withMessage("min 3 char")
+        .trim()
+        .isAlpha()
+        .withMessage("alphabets only!"),
     body("email")
         .isEmail()
         .normalizeEmail()
         .withMessage("Must Be a Valid Email Address"),
-    body("telephone").isLength({ min: 2 }).isNumeric(),
+    body("telephone")
+        .isLength({ min: 2 })
+        .withMessage("min length 2")
+        .isNumeric()
+        .withMessage("only numbers"),
     body("country"),
-    body("message").isLength({ max: 100 }),
+    body("message")
+        .isLength({ max: 100 })
+        .withMessage("max length of 100 char reached!"),
 ];
 
 export default rules;
