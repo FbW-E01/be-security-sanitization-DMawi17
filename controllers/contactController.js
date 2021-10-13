a8c20088a5735e429362c29c3c2a74f0a10d55ed
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-13 20:28:01 +0200

    --amend

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,16 +1,19 @@
-import express from 'express';
+import express from "express";
 import Contact from "../models/contact.js";
+import errChecks from "../errorChecks.js";
+import rules from "../validation.js";
+
 const router = express.Router();
 
 // Besides adding validation and sanitation
 // feel free to improve these in other ways!
 
-router.post("/", async (request, response) => {
+router.post("/", errChecks(rules), async (request, response) => {
     response.json(await Contact.create(request.body));
 });
 
 router.get("/", async (request, response) => {
-    response.json(await Contact.find({}));
+    response.json(await Contact.find());
 });
 
 export default router;
