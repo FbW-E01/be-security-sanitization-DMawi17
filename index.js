2a5e70416e823ffc93199512946d8d6b31b1887f
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2021-10-14 08:28:37 +0200

    modify index and remove history

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from "express";
 import config from "./config.js";
 import connect from "./database.js";
-// import errorController from "./controllers/errorController.js";
-// import contactController from "./controllers/contactController.js";
 import contactRoute from "./routes/contact.route.js";
 import errorCtrl from "./helpers/error.controller.js";
 
@@ -12,8 +10,7 @@ await connect();
 
 // What do you think, is /contact better or /contacts?
 // Research opinions about this after you are done with the task
-// server.use("/contact", contactController);
-// server.use(errorController);
+
 server.use("/contact", contactRoute);
 server.use(errorCtrl);
 
